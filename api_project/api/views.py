a7af2f69409b52161dbb896e516c4f19cb2594b4
Maryem FATIMI <37277665+Maryem-FATIMI@users.noreply.github.com>
2024-11-24 23:33:22 +0100

    second commit task 2 of the DRF module

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework import permissions
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, BasePermission
 
 class BookListAPIView(generics.ListAPIView):
@@ -14,6 +15,15 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated, IsAdminUser]
+
+class BookViewSet(viewsets.ModelViewSet):
+
+    # Restrict the view to only authenticated users
+    permission_classes = [permissions.IsAuthenticated]
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
 
 class IsAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
--- api_project/api_project/api/views.py
+++ api_project/api_project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework import permissions
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, BasePermission
 
 class BookListAPIView(generics.ListAPIView):
@@ -15,9 +16,23 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated, IsAdminUser]
 
+class BookViewSet(viewsets.ModelViewSet):
+
+    # Restrict the view to only authenticated users
+    permission_classes = [permissions.IsAuthenticated]
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
 class IsAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in ['GET', 'HEAD', 'OPTIONS']:
             return True
         return obj.author == request.user
     permission_class = [IsAuthenticated]
+
+
+
+
+

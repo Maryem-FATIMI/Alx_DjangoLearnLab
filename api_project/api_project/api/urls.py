b0a45ca760583583689c945c05edf6649dccd7d7
Maryem FATIMI <37277665+Maryem-FATIMI@users.noreply.github.com>
2024-11-24 22:57:54 +0100

    second commit task 1 of the DRF module

--- api_project/api_project/api/urls.py
+++ api_project/api_project/api/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 from .views import BookListAPIView
+from django.urls import path, include
+#from .views import BookList
+from .views import BookViewSet
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('books/', BookListAPIView.as_view(), name='book-list'),  # Maps to the BookList view
+ path('api-token-auth/', obtain_auth_token, name='api-token-auth')
 ]

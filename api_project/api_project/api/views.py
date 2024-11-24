93e27457238a0ea1209405a65f0197ccfa6153f9
Maryem FATIMI <37277665+Maryem-FATIMI@users.noreply.github.com>
2024-11-24 23:20:09 +0100

    second commit task 1 of the DRF module

--- api_project/api_project/api/views.py
+++ api_project/api_project/api/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
 
 class BookListAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
